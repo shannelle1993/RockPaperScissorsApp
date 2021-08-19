016ac120200499da80bb00c2c9843b5bcf3110e1
Shannelle Counsell <67432903+shannelle1993@users.noreply.github.com>
2021-08-19 17:44:55 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const userScore_span = document.getElementById("user-score")
 const computerScore_span = document.getElementById("computer-score")
 const scoreBoard_div = document.querySelector(".score-board")
 const result_p = document.querySelector(".result > p")
-const rock_div = document.getElementById("rock")
-const paper_div = document.getElementById("paper")
-const scissors_div = document.getElementById("scissors")
+const rock_div = document.getElementById("Rock")
+const paper_div = document.getElementById("Paper")
+const scissors_div = document.getElementById("Scissors")
 
 function getComputerChoice() {
     const choices = ['Rock','Paper','Scissors']
